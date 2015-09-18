152b515ccf48d7c811e22c3da3b571ed98a966d9
williamking <williamjwking@gmail.com>
2015-09-18 15:05:01 +0800

    ...

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: "Welcome to my home!" });
+    res.render('index', { title: "Welcome to my home!" });
+});
+
+/* Receive the article data and save it */
+router.post('/article', function(req, res) {
+    
 });
 
 module.exports = router;

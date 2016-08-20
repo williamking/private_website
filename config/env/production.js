bb6a8d691f3b259ca4e8616b067fdcbdfc1b7441
william <williamjwking@gmail.com>
2016-08-20 22:23:30 +0800

    update

--- config/env/production.js
+++ config/env/production.js
@@ -4,11 +4,11 @@ const path = require('path');
 module.exports = {
 	administrator: '$2a$05$4VIAk/kXnT2sTv5.1LWkuOySXY0vXnHIzzNLDG6CVkprmmBJPAjKu',
 	saltNum: 10,
-	port: 80,
+	port: 443,
 	database: 'mongodb://localhost/website',
 	spdy_options: {
-      key: fs.readFileSync(path.join(__dirname, '../../cert/production/1_zone.williamking.cn_cert.crt')),
-      cert: fs.readFileSync(path.join(__dirname, '../../cert/production/2_zone.williamking.cn.key')),
+      cert: fs.readFileSync(path.join(__dirname, '../../cert/production/1_zone.williamking.cn_cert.crt')),
+      key: fs.readFileSync(path.join(__dirname, '../../cert/production/2_zone.williamking.cn.key')),
       spdy: {
         protocols: ['h2', 'spdy/3.1', 'spdy/3', 'spdy/2', 'http/1.1', 'http/1.0'],
         plain: false,
